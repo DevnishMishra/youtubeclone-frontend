0d41a6a46b77d89264d167ac77da4715344fa408
DevnishMishra <devnishmishra@gmail.com>
2026-03-08 16:00:32 +0530

    Add responsive design for sidebar and video grid

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Sidebar({ isOpen }) {
   return (
     <aside
-      className={`bg-gray-100 w-64 p-4 fixed h-full top-0 left-0 transform ${
+      className={`bg-gray-100 w-64 p-4 fixed h-full top-0 left-0 transform md:translate-x-0 ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       } transition-transform duration-300 z-40`}
     >
